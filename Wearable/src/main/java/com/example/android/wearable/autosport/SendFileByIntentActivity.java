32da2531f975805fc58c71f2f25a207682306984
Pengyuan Zhou <zpymyyn@gmail.com>
2020-08-29 15:42:01 +0300

    sync files

---
+++ Wearable/src/main/java/com/example/android/wearable/autosport/SendFileByIntentActivity.java
@@ -0,0 +1,70 @@
+package com.example.android.wearable.autosport;
+
+import android.content.ComponentName;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SendFileByIntentActivity extends AppCompatActivity {
+
+    private static final int REQUEST_SELECT_FILE = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_send_file_by_intent);
+        
+        initViews();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case REQUEST_SELECT_FILE:
+                if (resultCode == RESULT_OK) {
+                    Uri uri = data.getData();
+                    sendFile(uri);
+                } else {
+                    Toast.makeText(this, getString(R.string.no_valid_file), Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void initViews() {
+        Button sendButton = (Button) findViewById(R.id.send);
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chooseFile();
+            }
+        });
+    }
+
+    private void chooseFile() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("*/*");
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
+
+        startActivityForResult(
+                Intent.createChooser(intent, "Select file"),
+                REQUEST_SELECT_FILE
+        );
+    }
+
+    private void sendFile(Uri uri) {
+        Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+        sharingIntent.setType("plain/text");
+        sharingIntent.setComponent(new ComponentName("com.android.bluetooth", "com.android.bluetooth.opp.BluetoothOppLauncherActivity"));
+        sharingIntent.putExtra(Intent.EXTRA_STREAM, uri);
+        startActivity(sharingIntent);
+    }
+}
