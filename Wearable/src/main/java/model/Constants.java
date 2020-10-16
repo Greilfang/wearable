ec45b44bc1158b70a15353b313c2d627c08febaa
Greilfang <565222945@qq.com>
2020-10-16 20:42:41 +0800

    try try

---
+++ Wearable/src/main/java/model/Constants.java
@@ -0,0 +1,14 @@
+package model;
+
+public class Constants {
+
+    public static String[] MLP_CLASSES = new String[]{
+            "lying on back and right side",
+            "ascending and descending stairs",
+            "exercising on a stepper",
+            "exercising on a cross trainer",
+            "exercising on an exercise bike in horizontal positions",
+            "exercising on an exercise bike in vertical positions"
+
+    };
+}
